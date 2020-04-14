f9112468b3195bbf8b47f2ebc3ba1f6ded36dff8
Philipnah <Philip@han.dk>
2020-04-14 14:28:09 +0200

    Fixes, requires too much RAM after ~12 letters.

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-# get dictionary
-
 from itertools import permutations 
-
 
 def Assembly(letters):
      # We put each character from the input string into a list.
@@ -11,11 +8,15 @@ def Assembly(letters):
 
 
      
-     # Get all permutations of length of "lenght"
+     # Get all permutations of length of "lenght" if the lenght of the word is only one character it wont count it
      y = 0
      tupleList = []
      while y < len(letterList):
           lenght = len(letterList) - y
+          
+          if lenght <= 1:
+               break
+          
           perm = permutations(letterList, lenght) 
           
           # we put all the obtained permutations in a list
@@ -44,20 +45,12 @@ results = Assembly(myInput)
 # We need to import and read the dictionary file and compare the strings
 
 with open("dict.txt", "r") as file:
-     dictList = file.readlines()
-
-x = 0
-while x < len(results):
-     z = 0
-     while z < len(dictList):
-          if results[x] == dictList[z]:
-               print("Result: " + results[x])
-               break
-          else:
-               z += 1
-
-     print(results[x])
-     x += 1
+     dictList = []
+     for line in file:
+          line = line.rstrip()
+          dictList.append(line)
      
 
-# select only line containing letters
+print(list(set(results) & set(dictList)))
+
+# idea: select only line containing letters
